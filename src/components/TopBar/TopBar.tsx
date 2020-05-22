493dd952b3a60c764e4f836c3051c3ce129d1443
moustaxx <maciasm1303@gmail.com>
2020-05-22 18:53:53 +0200

    Add link to homepage when click site name

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './TopBar.module.css';
 
 const TopBar = () => {
     return (
         <div className={styles.root}>
-            MaChat
+            <Link to="/" className={styles.link}>MaChat</Link>
         </div>
     );
 };
