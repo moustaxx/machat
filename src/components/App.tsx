e91f2be1156289400efe9343659e24712632679d
moustaxx <maciasm1303@gmail.com>
2020-05-22 18:49:16 +0200

    Add redirection to /login when not logged in

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import TopBar from './TopBar';
 import MessageBox from './MessageBox';
 import LoginScreen from './LoginScreen';
-import PageNotFound from './PageNotFound/PageNotFound';
-// import LeftSideBar from './LeftSideBar';
+import PageNotFound from './PageNotFound';
 
 import styles from './App.module.css';
+import { SettingsContext } from '../contexts/SettingsContext';
+
+const protectedRoute = (component: JSX.Element, condition: string | boolean | null) => {
+    return condition ? component : <Navigate to="login" replace />;
+};
 
 const App = () => {
+    const { nickname } = useContext(SettingsContext).settings;
+
     return (
         <div className={styles.root}>
-            {/* <LeftSideBar /> */}
             <div className={styles.container}>
                 <TopBar />
                 <Routes>
-                    <Route path="/" element={<MessageBox />} />
-                    <Route path="/404" element={<PageNotFound />} />
+                    <Route path="/" element={protectedRoute(<MessageBox />, nickname)} />
+                    <Route path="404" element={<PageNotFound />} />
                     <Route path="login" element={<LoginScreen />} />
                     <Route path="*" element={<Navigate to="/404" replace />} />
                 </Routes>
