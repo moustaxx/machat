6f15eecc8b01b937e40a20aa2428a179b17cc6ed
moustaxx <maciasm1303@gmail.com>
2020-05-28 19:40:04 +0200

    Add ErrorBoundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type TProps = {
+    children: React.ReactNode;
+    fallback: React.ReactNode;
+};
+
+type TState = {
+    error: Error | null;
+};
+
+export default class ErrorBoundary extends React.Component<TProps, TState> {
+    constructor(props: TProps) {
+        super(props);
+        this.state = { error: null };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        return { error };
+    }
+
+    render() {
+        const { fallback, children } = this.props;
+        const { error } = this.state;
+
+        if (error) return fallback;
+        return children;
+    }
+}
