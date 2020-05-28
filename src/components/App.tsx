cb3dee6ecd1585d78b7be2645e78ae8639938795
moustaxx <maciasm1303@gmail.com>
2020-05-28 20:09:39 +0200

    Show only IndexScreen if not logged in

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,8 +8,12 @@ import PageNotFound from './PageNotFound';
 import styles from './App.module.css';
 import { SettingsContext } from '../contexts/SettingsContext';
 
-const protectedRoute = (component: JSX.Element, condition: string | boolean | null) => {
-    return condition ? component : <Navigate to="/" replace />;
+const protectedRoute = (
+    component: JSX.Element,
+    condition: string | boolean | null,
+    fallback = '/',
+) => {
+    return condition ? component : <Navigate to={fallback} replace />;
 };
 
 const App = () => {
@@ -18,10 +22,29 @@ const App = () => {
     return (
         <div className={styles.root}>
             <Routes>
-                <Route path="/" element={<IndexScreen />} />
-                <Route path="app" element={protectedRoute(<MessageBox />, nickname)} />
-                <Route path="404" element={<PageNotFound />} />
-                <Route path="*" element={<Navigate to="/404" replace />} />
+                <Route
+                    path="/"
+                    element={nickname
+                        ? <Navigate to="/app" replace />
+                        : <Navigate to="/welcome" replace />
+                    }
+                />
+                <Route
+                    path="welcome"
+                    element={<IndexScreen />}
+                />
+                <Route
+                    path="app"
+                    element={protectedRoute(<MessageBox />, nickname)}
+                />
+                <Route
+                    path="404"
+                    element={<PageNotFound />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/404" replace />}
+                />
             </Routes>
         </div>
     );
