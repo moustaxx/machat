da88f7eeb085ed0359d6a7664a1029eb988a52ad
moustaxx <maciasm1303@gmail.com>
2020-06-30 21:56:58 +0200

    Add Relay & turn on React suspense mode
    Upgrade React to experimental

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { unstable_createRoot as createRoot } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 import ApolloProvider from './providers/ApolloProvider';
+import RelayProvider from './providers/RelayProvider';
 import SettingsProvider from './contexts/SettingsContext';
 import App from './components/App';
 
+const root = document.getElementById('root');
+if (!root) throw new Error('no root');
 
-
-ReactDOM.render(
+createRoot(root).render(
     <BrowserRouter>
         <ApolloProvider>
-            <SettingsProvider>
-                <App />
-            </SettingsProvider>
+            <RelayProvider>
+                <SettingsProvider>
+                    <App />
+                </SettingsProvider>
+            </RelayProvider>
         </ApolloProvider>
     </BrowserRouter>,
-    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/providers/RelayProvider.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+    FetchFunction,
+    GraphQLSingularResponse,
+    SubscribeFunction,
+    Observable,
+    Environment as RelayEnvironment,
+    Network,
+    Store,
+    RecordSource,
+} from 'relay-runtime';
+import { RelayEnvironmentProvider as EnvironmentProvider } from 'react-relay/hooks';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+const fetchFunction: FetchFunction = async (operation, variables) => {
+    try {
+        const response = await fetch('https://machat-server.herokuapp.com/v1/relay', {
+            method: 'POST',
+            credentials: 'include',
+            keepalive: true,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: operation.text,
+                variables,
+            }),
+        });
+
+        // Use GraphQLSingularResponse instead of GraphQLResponse
+        // because we are not using request batching
+        const json = await response.json() as GraphQLSingularResponse;
+
+        if ('errors' in json && Array.isArray(json.errors)) {
+            json.errors.map((error) => (
+                console.error(
+                    `GraphQL Error: ${error.message}\n`,
+                    'Details:', { error, operation, variables },
+                )
+            ));
+
+            throw new Error(json.errors[0]?.message || 'Unknown');
+        }
+
+        return json;
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+const subscriptionClient = new SubscriptionClient(
+    'wss://machat-server.herokuapp.com/v1/relay',
+    {
+        reconnect: true,
+        connectionCallback: (error, res) => {
+            if (!error) return;
+            console.error('WebSocket Error:', error, res);
+        },
+    },
+);
+
+const subscribeFunction: SubscribeFunction = (request, variables) => {
+    return Observable.create((sink) => {
+        const subscription = subscriptionClient
+            .request({
+                operationName: request.name,
+                query: request.text || undefined,
+                variables,
+            })
+            .subscribe(sink as any);
+
+        return () => subscription.unsubscribe();
+    });
+};
+
+export const Environment = new RelayEnvironment({
+    network: Network.create(fetchFunction, subscribeFunction),
+    store: new Store(new RecordSource(), {
+        gcReleaseBufferSize: 10,
+    }),
+});
+
+const RelayProvider: React.FC = ({ children }) => (
+    <EnvironmentProvider environment={Environment}>
+        {children}
+    </EnvironmentProvider>
+);
+
+export default RelayProvider;
