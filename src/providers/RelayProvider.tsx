76b1b4eacf561c132ff544fc81ca4d6c4edcce35
moustaxx <maciasm1303@gmail.com>
2021-05-06 19:32:32 +0200

    Clean RelayProvider file, add NamedError

--- src/providers/RelayProvider.tsx
+++ src/providers/RelayProvider.tsx
@@ -3,53 +3,62 @@ import {
     GraphQLSingularResponse,
     SubscribeFunction,
     Observable,
-    Environment as RelayEnvironment,
+    Environment,
     Network,
     Store,
     RecordSource,
+    PayloadError,
 } from 'relay-runtime';
-import { RelayEnvironmentProvider as EnvironmentProvider } from 'react-relay/hooks';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-const fetchFunction: FetchFunction = async (operation, variables) => {
-    try {
-        const response = await fetch('http://localhost:4000/graphql', {
-            method: 'POST',
-            credentials: 'include',
-            keepalive: true,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                query: operation.text,
-                variables,
-            }),
-        });
+class NamedError extends Error {
+    code!: string;
 
-        // Use GraphQLSingularResponse instead of GraphQLResponse
-        // because we are not using request batching
-        const json = await response.json() as GraphQLSingularResponse;
-
-        if ('errors' in json && Array.isArray(json.errors)) {
-            json.errors.map((error) => (
-                console.error(
-                    `GraphQL Error: ${error.message}\n`,
-                    'Details:', { error, operation, variables },
-                )
-            ));
-
-            throw new Error(json.errors[0]?.message || 'Unknown');
-        }
-
-        return json;
-    } catch (err) {
-        throw new Error(err);
+    constructor(message: string, name: string) {
+        super();
+        this.message = message;
+        this.name = `${name}`;
     }
+}
+
+const fetchFunction: FetchFunction = async (operation, variables) => {
+    const response = await fetch('http://localhost:4000/graphql', {
+        method: 'POST',
+        credentials: 'include',
+        keepalive: true,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables,
+        }),
+    });
+
+    // Use GraphQLSingularResponse instead of GraphQLResponse
+    // because we are not using request batching
+    const json = await response.json() as GraphQLSingularResponse;
+
+    if ('errors' in json && Array.isArray(json.errors)) {
+        json.errors.forEach((error) => (
+            console.error(
+                `GraphQL Error: ${error.message}\n`,
+                'Details:', { error, operation, variables },
+            )
+        ));
+
+        const err = json.errors[0] as PayloadError & { extensions?: { code: string } };
+        throw new NamedError(err.message || 'Unknown', err.extensions?.code || 'Error');
+    }
+
+    return json;
 };
 
 const subscriptionClient = new SubscriptionClient(
     'ws://localhost:4000/graphql',
     {
+        // lazy: true, // set to true when not logged in
         reconnect: true,
         connectionCallback: (error: Error[] | undefined, res) => {
             if (!error) return;
@@ -72,17 +81,15 @@ const subscribeFunction: SubscribeFunction = (request, variables) => {
     });
 };
 
-export const Environment = new RelayEnvironment({
+export const environment = new Environment({
     network: Network.create(fetchFunction, subscribeFunction),
-    store: new Store(new RecordSource(), {
-        gcReleaseBufferSize: 10,
-    }),
+    store: new Store(new RecordSource()),
 });
 
 const RelayProvider: React.FC = ({ children }) => (
-    <EnvironmentProvider environment={Environment}>
+    <RelayEnvironmentProvider environment={environment}>
         {children}
-    </EnvironmentProvider>
+    </RelayEnvironmentProvider>
 );
 
 export default RelayProvider;
