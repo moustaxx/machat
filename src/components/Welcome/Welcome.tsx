e5bc22c89b01056c882590e2f1a355b1b2e6e241
moustaxx <maciasm1303@gmail.com>
2021-05-06 18:47:38 +0200

    Revert Welcome screen's get in button

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { GoMarkGithub } from 'react-icons/go';
 
 import styles from './Welcome.module.css';
 import Button from '../Button';
+import { SettingsContext } from '../../contexts/SettingsContext';
 
 type TProps = {
     setPanel: (name: 'welcome' | 'login' | 'register') => void;
 };
 
 const Welcome = ({ setPanel }: TProps) => {
+    const navigate = useNavigate();
+    const { settings } = useContext(SettingsContext);
+
     const footer = (
         <div className={styles.footer}>
             Made by&nbsp;
@@ -29,18 +35,31 @@ const Welcome = ({ setPanel }: TProps) => {
                 with your friends and many more! All for free!
             </p>
             <div className={styles.btnWrapper}>
-                <Button
-                    mode="light"
-                    className={styles.btn}
-                    onClick={() => setPanel('register')}
-                >Sign up
-                </Button>
-                <Button
-                    mode="light"
-                    className={styles.btn}
-                    onClick={() => setPanel('login')}
-                >Log in
-                </Button>
+                {settings.isLoggedIn
+                    ? (
+                        <Button
+                            mode="light"
+                            className={styles.btn}
+                            onClick={() => navigate('/app')}
+                        >Get in
+                        </Button>
+                    ) : (
+                        <>
+                            <Button
+                                mode="light"
+                                className={styles.btn}
+                                onClick={() => setPanel('register')}
+                            >Sign up
+                            </Button>
+                            <Button
+                                mode="light"
+                                className={styles.btn}
+                                onClick={() => setPanel('login')}
+                            >Log in
+                            </Button>
+                        </>
+                    )
+                }
             </div>
             {footer}
         </div>
